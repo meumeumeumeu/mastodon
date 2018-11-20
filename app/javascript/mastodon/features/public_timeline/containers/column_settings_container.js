f4ec70add43ab1b1a3387ee382f844b9bdceaa17
meumeumeumeu <44948009+meumeumeumeu@users.noreply.github.com>
2018-11-20 16:46:59 +0900

    Update column_settings_container.js
    こうらしい

--- app/javascript/mastodon/features/public_timeline/containers/column_settings_container.js
+++ app/javascript/mastodon/features/public_timeline/containers/column_settings_container.js
@@ -7,15 +7,16 @@ const mapStateToProps = (state, { columnId }) => {
   const uuid = columnId;
   const columns = state.getIn(['settings', 'columns']);
   const index = columns.findIndex(c => c.get('uuid') === uuid)};
-const mapStateToProps = state => ({
-  settings: state.getIn(['settings', 'public']),
-  highlight_keywords: state.get('highlight_keywords'),
-});
 
   return {
     settings: (uuid && index >= 0) ? columns.get(index).get('params') : state.getIn(['settings', 'public']),
   };
 };
+
+const mapStateToProps = state => ({
+  settings: state.getIn(['settings', 'public']),
+  highlight_keywords: state.get('highlight_keywords'),
+});
 
 const mapDispatchToProps = (dispatch, { columnId }) => {
   return {
